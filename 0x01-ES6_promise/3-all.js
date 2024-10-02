085bebc5fe541d19bb5c0164b081b7d737c4d641
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-10-03 00:43:26 +0200

    rewrite consol.log not string literals

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ const { uploadPhoto, createUser } = require('./utils');
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((ms) => {
-    console.log(`${ms[0].body} ${ms[1].firstName} ${ms[1].lastName}`);
+    console.log(ms[0].body, ms[1].firstName, ms[1].lastName);
   }).catch(() => {
     console.log(Error('Signup system offline'));
   });
