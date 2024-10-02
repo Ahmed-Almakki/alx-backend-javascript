4f0d8c06a12067d23fdc7e953692cfd8b5e6d59b
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-10-03 01:31:10 +0200

    async withot await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,7 @@
+const { uploadPhoto, createUser } = require('./utils');
+
+export default async function asyncUploadUser() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((msg) => ({ photo: msg[0], user: msg[1] }))
+    .catch(() => ({ photo: null, user: null }));
+}
