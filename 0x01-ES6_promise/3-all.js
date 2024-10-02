999c044d8aec9424b14793a15422e420970af938
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-10-03 00:44:58 +0200

    previous way

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,8 @@ const { uploadPhoto, createUser } = require('./utils');
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((ms) => {
-    console.log(ms[0].body, ms[1].firstName, ms[1].lastName);
+    console.log(`${ms[0].body} ${ms[1].firstName} ${ms[1].lastName}`);
   }).catch(() => {
-    console.log(Error('Signup system offline'));
+    console.log('Signup system offline');
   });
 }
