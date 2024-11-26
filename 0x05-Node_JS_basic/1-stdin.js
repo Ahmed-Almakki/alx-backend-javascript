505c44c660a60cef6754d712afa1b1b11117acbe
Ahmed-Almakkii <ahmedlmakki@gmail.com>
2024-11-26 20:40:42 +0000

    check if streamd

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,14 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
-  process.on('exit', () => {
-    console.log('This important software is now closing');
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    console.log(`Your name is: ${data.toString().trim()}`);
+    process.exit();
   });
-});
+} else {
+  process.stdin.on('data', (data) => {
+    console.log(`Your name is: ${data.toString().trim()}`);  
+    process.on('exit', () => {
+      console.log('This important software is now closing');
+    });
+  });
+}
