9429a72bf8e8b29aae18871cef99b2e43d00a3d1
Ahmed-Almakkii <ahmedlmakki@gmail.com>
2024-11-26 20:44:55 +0000

    process instead of log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 console.log('Welcome to Holberton School, what is your name?');
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data.toString().trim()}`);
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
 } else {
   process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data.toString().trim()}`);  
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.on('exit', () => {
       console.log('This important software is now closing');
     });
